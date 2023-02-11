2a507305471aaaaf1f18b3193209954636f5296c
unknown <doniaskima344@gmail.com>
2023-02-11 22:57:57 +0100

    feat(model):Point model

---
+++ models/Point.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const pointSchema = new mongoose.Schema({
+    title: String,
+    club: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "club",
+        required: true,
+    },
+    value: {
+        type: Number,
+        default: 0,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    },
+}, { timestamps: true });
+
+const Point = mongoose.model("points", pointSchema);
+module.exports = Point;
