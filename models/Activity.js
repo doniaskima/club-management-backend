35e1fc146c2497fede616d4803cfe02f29c706c7
unknown <doniaskima344@gmail.com>
2023-02-11 23:46:55 +0100

    feat(model):add Activity component

---
+++ models/Activity.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const activitySchema = new mongoose.Schema({
+    club: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "club",
+        required: true,
+    },
+    title: String,
+    startDate: Date,
+    endDate: Date,
+    boards: [{
+        title: String,
+        cards: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "activityCards",
+        }, ],
+    }, ],
+    collaborators: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+    }, ],
+    sumary: {
+        type: String,
+        default: "",
+    },
+    joinPoint: {
+        type: Number,
+        default: 0,
+    },
+    configType: {
+        type: String,
+        default: "percent", // percent/quantity
+    },
+    configMilestone: [{
+        percentOrQuantity: Number,
+        point: Number,
+    }, ],
+}, { timestamps: true });
+
+const Activity = mongoose.model("activities", activitySchema);
+module.exports = Activity;
---
+++ models/ActivityCard.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+const Activity = require("./Activity");
+const cloudinary = require("../helper/Cloudinary");
