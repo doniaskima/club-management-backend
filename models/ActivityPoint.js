cfdab3b9064ce7766e775299c34e0ce82e7a90ba
unknown <doniaskima344@gmail.com>
2023-02-11 23:34:15 +0100

    feat(model): ActivityPoint model

---
+++ models/ActivityPoint.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const activityPointSchema = new mongoose.Schema({
+    title: String,
+    activity: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "activities",
+        required: true,
+    },
+    value: {
+        type: Number,
+        default: 0,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    },
+}, { timestamps: true });
+
+const ActivityPoint = mongoose.model("activityPoints", activityPointSchema);
+module.exports = ActivityPoint;
