e0e75c11d04a0a5442798f47404c336a4aec30d6
unknown <doniaskima344@gmail.com>
2023-02-11 22:51:16 +0100

    feat(model):Message model

---
+++ models/Message.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    original_filename: String,
+    room_id: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+const Message = mongoose.model("message", messageSchema);
+module.exports = Message;
