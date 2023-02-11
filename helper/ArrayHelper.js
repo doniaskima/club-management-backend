cb81d66e2e218fb3a66f7979155c6f58b566daec
unknown <doniaskima344@gmail.com>
2023-02-12 00:26:20 +0100

    feat(model):add ActivityCard

---
+++ helper/ArrayHelper.js
@@ -0,0 +1,62 @@
+function isElementInArray(key, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].toString() === key.toString()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isElementInArrayObject(key, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]._id.toString() === key) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+function elementInArrayObject(key, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]._id.toString() === key.toString()) {
+            return {
+                element: array[i],
+                index: i,
+            };
+        }
+    }
+    return {
+        element: null,
+        index: -1,
+    };
+}
+
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+function uniqueArray(array) {
+    const unique = array.filter(onlyUnique);
+    return unique;
+}
+
+function uniqueIdObjArray(array) {
+    return [...new Map(array.map((item) => [item["_id"], item])).values()];
+}
+
+function notContainsNullArray(array) {
+    return array.filter((value) => {
+        return value !== null;
+    });
+}
+
+module.exports = {
+    isElementInArray,
+    isElementInArrayObject,
+    uniqueArray,
+    uniqueIdObjArray,
+    notContainsNullArray,
+    elementInArrayObject,
+};
--- models/ActivityCard.js
+++ models/ActivityCard.js
@@ -1,3 +1,70 @@
 const mongoose = require("mongoose");
 const Activity = require("./Activity");
 const cloudinary = require("../helper/Cloudinary");
+const async = require("async");
+const { isElementInArray } = require("../helper/ArrayHelper");
+
+const activityCardSchema = new mongoose.Schema({
+    activity: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "activities",
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    files: [{
+        original_filename: String,
+        url: String,
+        public_id: String,
+    }, ],
+    userJoin: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+    }, ],
+    groupJoin: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "group",
+    }, ],
+    comments: [{
+        content: String,
+        createdAt: Date,
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+        },
+    }, ],
+    pointValue: {
+        type: Number,
+        default: 0,
+    },
+}, { timestamps: true });
+
+
+activityCardSchema.pre("remove", function(next) {
+    var card = this;
+
+    async.forEach(
+        card.files,
+        async function(item, callback) {
+            await cloudinary.uploader.destroy(item.public_id).catch((err) => {
+                console.log("destroy image err ", err.message);
+            });
+            if (typeof callback === "function") {
+                return callback();
+            }
+        },
+        function(err) {
+            if (err) {
+                throw Error("Card delete err - " + err.message);
+            }
+        }
+    );
+
+    next();
+});
+
+
+const ActivityCard = mongoose.model("activityCards", activityCardSchema);
+module.exports = ActivityCard;
