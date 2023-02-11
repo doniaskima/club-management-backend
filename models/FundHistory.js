d7b223f50750b9bd746ae35b4c2e347719d0af73
unknown <doniaskima344@gmail.com>
2023-02-11 23:53:07 +0100

    feat(model):FundHistory

---
+++ models/FundHistory.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const fundHistorySchema = new mongoose.Schema({
+    club: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "club",
+        required: true,
+    },
+    content: String,
+    type: {
+        type: String,
+        require: true,
+    },
+    total: {
+        type: Number,
+        default: 0,
+    },
+    file_url: String,
+    cloudinary_id: String,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    },
+    submitted: [{
+        member_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+        },
+        total: {
+            type: Number,
+            default: 0,
+        },
+        submittedAt: {
+            type: Date,
+            default: Date.now,
+        },
+    }, ],
+}, { timestamps: true });
+
+const FundHistory = mongoose.model("fundHistory", fundHistorySchema);
+module.exports = FundHistory;
