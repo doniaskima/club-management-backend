9762e8dc4e9f1659b06430acee2751ae7784457a
unknown <doniaskima344@gmail.com>
2023-02-28 23:32:45 +0100

    fix routes

--- routes/activity.route.js
+++ routes/activity.route.js
@@ -8,5 +8,9 @@ router.post("/createcard", activityController.createCard);
 router.get("/list/:clubId", activityController.getList);
 router.get("/one/:activityId", activityController.getOne);
 router.get("/collaborators/:activityId", activityController.getCollaborators);
+router.get('/usersnotcollaborators/:activityId', activityController.getUsersNotCollaborators)
+router.get('/search/:clubId/:searchValue', activityController.search)
+router.get('/searchcollaborators/:activityId/:searchValue', activityController.searchCollaborators)
+router.get('/searchusersnotcollaborators/:activityId/:searchValue', activityController.searchUsersNotCollaborators)
 
 module.exports = router;
