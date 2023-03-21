ff7ed742a0dc0924f7fc342d99b153dd3d4fc514
unknown <doniaskima344@gmail.com>
2023-03-21 01:54:24 +0100

    fixing

--- controllers/groupControllers.js
+++ controllers/groupControllers.js
@@ -123,3 +123,64 @@ module.exports.getAllMembers = (req, res) => {
         res.status(500).json({ error: err.message })
     })
 }
+
+
+module.exports.delete = (req, res) => {
+    const groupId = req.params.groupId;
+
+    Group.findByIdAndDelete(groupId)
+    .then(() => {
+        res.status(200).send(groupId)
+    }).catch(err => {
+        res.status(500).send({ error: err.message })
+    })
+}
+
+module.exports.update = (req, res) => {
+    const groupId = req.params.groupId;
+    const { newName, membersRemove } = req.body;
+
+    Group.updateOne(
+        { _id: groupId },
+        {
+            $set: { name: newName },
+            $pull: { members: { $in: membersRemove } },
+        }
+    ).then(() => {
+        Group.findById(groupId)
+        .populate('members')
+        .then(result => {
+            res.status(200).send(result)
+        }).catch(err => {
+            res.status(400).send({ error: err.message })
+        })
+    }).catch(err => {
+        res.status(400).send({ error: err.message })
+    })
+}
+
+module.exports.getAllNotInGroup = async (req , res)=>{
+    const groupId = req.params.groupId ;
+    try {
+        const group = await Group.findById(groupId)
+        const club = await Club.findById(group.club)
+        let arrId = club.members
+        arrId.push(club.leader)
+        arrId.push(club.treasurer)
+
+        const listUser = await User.find({
+            $and: [
+                { _id: { $in: arrId } },
+                { _id: { $nin: group.members } }
+            ]
+        })
+
+        if (listUser) {
+            res.status(200).send(listUser)
+        } else {
+            res.status(500).json({ error: 'load list user failed' })
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
